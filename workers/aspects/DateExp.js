4bfe08a193e28385fa4bb6b76da88ca510dafa9b
Nathaniel Anderson <theredpea@gmail.com>
2013-09-09 23:03:09 -0600

    Almost done

--- workers/aspects/DateExp.js
+++ workers/aspects/DateExp.js
@@ -3,9 +3,12 @@ DateExp.ISO_8601 	= new RegExp('^([\+-]?\d{4}(?!\d{2}\b))'+ 		//[1](Optional sig
 								'((-?)((0[1-9]|1[0-2])' +			//[2]([3]Optional hyphen. [4]Optional([5]The month, 1-indexed
 									'(\3([12]\d|0[1-9]|3[01]))?' + 		//[6](\3 Optional hyphen, [7]The day of month))
 									'|W([0-4]\d|5[0-2])(-?[1-7])?' + 	//W (for "Week") [8](Week) [9]Optional(Optional hyphen, Day of Week)  	//r.exec('2009-W21-2T01:22')[8] == '21'
-									'|(00[1-9]|0[1-9]\d|[12]\d{2}|3([0-5]\d|6[1-6])))' + //[9](Day of year, up to 366 [10] Day after 300)
-'([T\s]((([01]\d|2[0-3])((:?)[0-5]\d)?|24\:?00)([\.,]\d+(?!:))?)?(\17[0-5]\d([\.,]\d+)?)?([zZ]|([\+-])([01]\d|2[0-3]):?([0-5]\d)?)?)?)?$/;
-');
+									'|(00[1-9]|0[1-9]\d|[12]\d{2}|3([0-5]\d|6[1-6])))' + 	//[10](Day of year, up to 366 [11] Day after 300)
+									'([T\s]((' 		+ 										//[12]T [13]([14](...
+									'([01]\d|2[0-3])((:?)[0-5]\d)?|24\:?00)'		+		//([15](Hour of the day)[16]Optional([17](Optional colon), Minute of the hour) (could be 24:00)
+									'([\.,]\d+(?!:))?)?'			+ 	//[17] Optional(Period or comma, and many digits not followed by colons)
+									'(\17[0-5]\d([\.,]\d+)?)?([zZ]|([\+-])([01]\d|2[0-3]):?([0-5]\d)?)?)?)?$/;
+									');
 
 //http://my.safaribooksonline.com/book/programming/regular-expressions/9780596802837/4dot-validation-and-formatting/id2983571
 DateExp.ISO_8601 = /^(?<year>-?(?:[1-9][0-9]*)?[0-9]{4})-(?<month>1[0-2]|0[1-9])-?
